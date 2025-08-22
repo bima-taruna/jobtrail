f577092d98e0dfed06a90a404fba558ee4bfe235
bima-taruna <bimataruna21@gmail.com>
2025-08-22 16:20:03 +0700

    feat: refactor job application routes to use async service methods and update schemas

--- src/job_application/routes.py
+++ src/job_application/routes.py
@@ -1,57 +1,54 @@
-from fastapi import APIRouter
-from src.job_application.schemas import JobApplication, UpdateJobApplication
+from fastapi import APIRouter, status, Depends
+from src.job_application.schemas import JobApplicationUpdateModel, JobApplicationCreateModel
 from fastapi.exceptions import HTTPException
-from src.job_application.job_application_data import job_applications
+from src.db.main import get_session
+from src.job_application.models import JobApplication
+from sqlmodel.ext.asyncio.session import AsyncSession
+from src.job_application.services import JobApplicationService
 job_application_router =  APIRouter()
-
+job_application_service = JobApplicationService()
 
 @job_application_router.get("/", response_model=list[JobApplication])
-def get_all_jobs():
+async def get_all_jobs(session:AsyncSession = Depends(get_session)):
+    job_applications = await job_application_service.get_all_jobs(session)
     return job_applications
 
 
 @job_application_router.get("/{id}")
-def get_job_by_id(id:int):
-    for job in job_applications:
-        if job.id == id:
-            return job
-    raise HTTPException(
-        status_code=404,
-        detail="Job not found"
-    )
+async def get_job_by_id(id:str, session:AsyncSession = Depends(get_session)):
+    job_application = await job_application_service.get_job(id, session)
+    if job_application is not None :
+        return job_application
+    else :
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Job not found"
+        )
 
-@job_application_router.post("/")
-def create_job_application(job_data:JobApplication) -> dict[str, list[JobApplication] | str | JobApplication] :
-    job_data.id = len(job_applications) + 1
-    job_applications.append(job_data)
-    return {
-        "message" : "200 item created succesfully",
-        "item" : job_data,
-        "result" : job_applications
-    }
+@job_application_router.post("/", status_code=status.HTTP_201_CREATED, response_model=JobApplication, response_model_exclude_none=True)
+async def create_job_application(job_data:JobApplicationCreateModel, session:AsyncSession = Depends(get_session)) :
+    job_application = await job_application_service.create_job(job_data, session)
+    return job_application
 
-@job_application_router.patch("/{id}")
-def update_job_application(id : int,job_data:UpdateJobApplication) :
-    update_data = job_data.model_dump(exclude_unset=True)
-    for job in job_applications:
-        if job.id == id:
-            for field, value in update_data.items():
-                setattr(job, field, value)
-            return job
-    raise HTTPException(
-        status_code=404,
-        detail="Job not found"
-    )
+@job_application_router.patch("/{id}", response_model_exclude_none=True)
+async def update_job_application(id : str,job_data:JobApplicationUpdateModel,session:AsyncSession = Depends(get_session)) :
+    update_data = await job_application_service.update_job(id,job_data,session)
     
-@job_application_router.delete("/{id}")
-def delete_job_application(id:int) :
-    for job in job_applications:
-        if job.id == id:
-            job_applications.remove(job)
-            return {
-                "message" : "selected item deleted"
-            }
-    raise HTTPException(
-        status_code=404,
-        detail="Job not found"
-    ) 
+    if update_data:
+        return update_data
+    else :
+        raise HTTPException(
+            status_code=404,
+            detail="Job not found"
+        )
+    
+@job_application_router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_job_application(id:str,session:AsyncSession = Depends(get_session) ) :
+    delete_data = await job_application_service.delete_job(id,session)
+    if delete_data:
+        return None
+    else :
+        raise HTTPException(
+            status_code=404,
+            detail="Job not found"
+        ) 
