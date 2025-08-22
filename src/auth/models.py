81b99dc1dbbdc66b2ca169f6259073ba97178f9f
bima-taruna <bimataruna21@gmail.com>
2025-08-22 21:35:22 +0700

    feat: add user model and user types schema for authentication

---
+++ src/auth/models.py
@@ -0,0 +1,28 @@
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+import uuid
+from .schemas import UserTypes
+from datetime import datetime, date
+class User(SQLModel, table=True) :
+    __tablename__ = 'users' # type: ignore[assignment]
+    id: uuid.UUID = Field(
+        sa_column=Column(
+          pg.UUID,
+          nullable=False,
+          primary_key=True,
+          default=uuid.uuid4  
+        )
+    )
+    username: str
+    password:str
+    email: str
+    first_name:str
+    last_name:str
+    is_verified:bool = Field(default=False)
+    user_type:UserTypes
+    created_at:datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at:datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+
+
+    def __repr__(self) -> str:
+        return f"<User {self.username}>"
---
+++ src/auth/schemas.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class UserTypes(Enum):
+    ADMIN = "admin"
+    USER = "user"
+    GUEST = "guest"
