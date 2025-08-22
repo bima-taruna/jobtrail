dec70c4143da5a3f62e840aedb54ae6e464cde99
bima-taruna <bimataruna21@gmail.com>
2025-08-22 16:20:10 +0700

    feat: update database initialization to use async engine and session management

--- src/db/main.py
+++ src/db/main.py
@@ -1,12 +1,26 @@
 from sqlmodel import create_engine, SQLModel
-from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlmodel.ext.asyncio.session import AsyncSession
+from typing import AsyncGenerator
+
 from src.config import Config
-engine = AsyncEngine(create_engine(
+
+async_engine = create_async_engine(
     url=Config.DATABASE_URL,
     echo=True
-))
+)
 
-async def init_db():
-    async with engine.begin() as conn:        
+async def init_db()->None:
+    async with async_engine.begin() as conn:        
         from src.job_application.models import JobApplication # type: ignore[unused-import]
         await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def get_session()-> AsyncGenerator[AsyncSession, None]:
+    Session = async_sessionmaker(
+        bind=async_engine,
+        class_=AsyncSession,
+        expire_on_commit=False
+    )
+    async with Session() as session:
+        yield session
