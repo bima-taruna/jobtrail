2f800c1fba6a43a79b69866eb3646515a8829ddc
bima-taruna <bimataruna21@gmail.com>
2025-08-24 15:20:02 +0700

    feat: update User model to use password_hash and define user_type with server default

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,5 +1,6 @@
 from sqlmodel import SQLModel, Field, Column
 import sqlalchemy.dialects.postgresql as pg
+from sqlalchemy import Enum as PgEnum
 import uuid
 from .schemas import UserTypes
 from datetime import datetime, date
@@ -14,12 +15,14 @@ class User(SQLModel, table=True) :
         )
     )
     username: str
-    password:str
+    password_hash:str = Field(exclude=True)
     email: str
     first_name:str
     last_name:str
     is_verified:bool = Field(default=False)
-    user_type:UserTypes
+    user_type:UserTypes = Field(
+        sa_column=Column(PgEnum(UserTypes, name="user_type"), nullable=False, server_default="USER")
+    )
     created_at:datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
     updated_at:datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
 
