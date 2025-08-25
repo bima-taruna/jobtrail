ef5aa760c3f80510c50345ef004d5925da7c086a
bima-taruna <bimataruna21@gmail.com>
2025-08-25 22:07:15 +0700

    feat: add JWT_SECRET and JWT_ALGORITHM to settings configuration

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str = "postgres:///default.db"
-    
+    JWT_SECRET:str = "secret"
+    JWT_ALGORITHM:str = "H256"
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore"
