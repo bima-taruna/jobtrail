4aef134d8f60ca73147c73b91bff7f4dacd84e24
bima-taruna <bimataruna21@gmail.com>
2025-08-31 21:28:09 +0700

    add route to get job application by user id

--- src/job_application/routes.py
+++ src/job_application/routes.py
@@ -11,15 +11,20 @@ access_token_bearer = AccessTokenBearer()
 job_application_router =  APIRouter()
 job_application_service = JobApplicationService()
 role_checker_standard = Depends(RoleChecker(['ADMIN', 'USER', 'GUEST']))
+role_checker_admin = Depends(RoleChecker(['ADMIN']))
 
-@job_application_router.get("/", response_model=list[JobApplication], dependencies=[role_checker_standard])
-async def get_all_jobs(session:AsyncSession = Depends(get_session), user_details=Depends(access_token_bearer)):
+@job_application_router.get("/", response_model=list[JobApplication], dependencies=[role_checker_admin])
+async def get_all_jobs(session:AsyncSession = Depends(get_session), token_details:dict=Depends(access_token_bearer)):
     job_applications = await job_application_service.get_all_jobs(session)
     return job_applications
 
+@job_application_router.get("/user/{user_id}", response_model=list[JobApplication], dependencies=[role_checker_standard])
+async def get_jobs_by_user_id(user_id:str,session:AsyncSession = Depends(get_session), token_details:dict=Depends(access_token_bearer)):
+    job_applications = await job_application_service.get_user_jobs(user_id,session)
+    return job_applications
 
 @job_application_router.get("/{id}", dependencies=[role_checker_standard])
-async def get_job_by_id(id:str, session:AsyncSession = Depends(get_session), user_details=Depends(access_token_bearer)):
+async def get_job_by_id(id:str, session:AsyncSession = Depends(get_session), token_details:dict=Depends(access_token_bearer)):
     job_application = await job_application_service.get_job(id, session)
     if job_application is not None :
         return job_application
@@ -30,12 +35,17 @@ async def get_job_by_id(id:str, session:AsyncSession = Depends(get_session), use
         )
 
 @job_application_router.post("/", status_code=status.HTTP_201_CREATED, response_model=JobApplication, response_model_exclude_none=True, dependencies=[role_checker_standard])
-async def create_job_application(job_data:JobApplicationCreateModel, session:AsyncSession = Depends(get_session), user_details=Depends(access_token_bearer)) :
-    job_application = await job_application_service.create_job(job_data, session)
+async def create_job_application(job_data:JobApplicationCreateModel, session:AsyncSession = Depends(get_session), token_details:dict=Depends(access_token_bearer)) :
+    user = token_details.get("user") if token_details else None
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    user_id = user["user_id"]
+    
+    job_application = await job_application_service.create_job(job_data,user_id, session)
     return job_application
 
 @job_application_router.patch("/{id}", response_model_exclude_none=True, dependencies=[role_checker_standard])
-async def update_job_application(id : str,job_data:JobApplicationUpdateModel,session:AsyncSession = Depends(get_session),user_details=Depends(access_token_bearer)) :
+async def update_job_application(id : str,job_data:JobApplicationUpdateModel,session:AsyncSession = Depends(get_session),token_details:dict=Depends(access_token_bearer)) :
     update_data = await job_application_service.update_job(id,job_data,session)
     
     if update_data:
@@ -47,7 +57,7 @@ async def update_job_application(id : str,job_data:JobApplicationUpdateModel,ses
         )
     
 @job_application_router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT, dependencies=[role_checker_standard])
-async def delete_job_application(id:str,session:AsyncSession = Depends(get_session),user_details=Depends(access_token_bearer) ) :
+async def delete_job_application(id:str,session:AsyncSession = Depends(get_session),token_details:dict=Depends(access_token_bearer) ) :
     delete_data = await job_application_service.delete_job(id,session)
     if delete_data:
         return None
