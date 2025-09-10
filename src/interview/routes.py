a6b3a74a1925029505405c276a18f0b231fde737
bima-taruna <bimataruna21@gmail.com>
2025-09-10 19:59:31 +0700

    create service and route for job interview

---
+++ src/interview/routes.py
@@ -0,0 +1,66 @@
+from fastapi import APIRouter, status, Depends
+from fastapi.exceptions import HTTPException
+from src.db.main import get_session
+from sqlmodel.ext.asyncio.session import AsyncSession
+from src.auth.dependencies import AccessTokenBearer, RoleChecker
+from .service import JobInterviewService
+from src.db.models import JobInterview
+from .schemas import JobInterviewCreateModel,JobInterviewUpdateModel
+
+access_token_bearer = AccessTokenBearer()
+job_interview_router = APIRouter(
+    prefix="/job-applications/{job_application_id}/interviews",
+    tags=["Job Interviews"]
+)
+job_interview_service = JobInterviewService()
+role_checker_standard = Depends(RoleChecker(['ADMIN', 'USER', 'GUEST']))
+role_checker_admin = Depends(RoleChecker(['ADMIN']))
+
+@job_interview_router.get("/", response_model=list[JobInterview], dependencies=[role_checker_standard])
+async def get_all_job_interviews(job_application_id:str,session:AsyncSession = Depends(get_session), token_details:dict=Depends(access_token_bearer)):
+    job_interviews =  await job_interview_service.get_all_interviews_by_app_id(job_application_id,session)
+    return job_interviews
+
+@job_interview_router.get("/{job_interview_id}", response_model=JobInterview, dependencies=[role_checker_standard])
+async def get_job_interview_by_id(job_application_id:str, job_interview_id:str, session:AsyncSession = Depends(get_session), token_details:dict=Depends(access_token_bearer)):
+    job_interview = job_interview_service.get_interviews_by_id(job_application_id,job_interview_id,session)
+    if job_interview is not None :
+        return job_interview
+    else :
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="interview not found"
+        )
+
+@job_interview_router.post("/", status_code=status.HTTP_201_CREATED, response_model=JobInterview, response_model_exclude_none=True, dependencies=[role_checker_standard])
+async def create_job_application(interview_data:JobInterviewCreateModel, session:AsyncSession = Depends(get_session), token_details:dict=Depends(access_token_bearer)) :
+    user = token_details.get("user") if token_details else None
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    user_id = user["user_id"]
+    
+    job_interview = await job_interview_service.create_job_interview(interview_data,user_id, session)
+    return job_interview
+
+@job_interview_router.patch("/{interview_id}", response_model_exclude_none=True, dependencies=[role_checker_standard])
+async def update_job_application(job_application_id:str,job_interview_id : str,interview_data:JobInterviewUpdateModel,session:AsyncSession = Depends(get_session),token_details:dict=Depends(access_token_bearer)) :
+    update_data = await job_interview_service.update_job_interview(job_application_id,job_interview_id,interview_data,session)
+    
+    if update_data:
+        return update_data
+    else :
+        raise HTTPException(
+            status_code=404,
+            detail="Interview not found"
+        )
+
+@job_interview_router.delete("/{interview_id}", status_code=status.HTTP_204_NO_CONTENT, dependencies=[role_checker_standard])
+async def delete_job_application(job_application_id:str,interview_id:str,session:AsyncSession = Depends(get_session),token_details:dict=Depends(access_token_bearer) ) :
+    delete_data = await job_interview_service.delete_job_interview(job_application_id, interview_id, session)
+    if delete_data:
+        return None
+    else :
+        raise HTTPException(
+            status_code=404,
+            detail="Interview not found"
+        ) 
---
+++ src/interview/service.py
@@ -0,0 +1,50 @@
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import select, desc
+from .schemas import JobInterviewCreateModel, JobInterviewUpdateModel
+from ..db.models import JobInterview
+from datetime import datetime
+import uuid
+class JobInterviewService:
+    async def get_all_interviews_by_app_id(self, job_application_id, session: AsyncSession):
+        statement = select(JobInterview).where(JobInterview.job_application_id == job_application_id, JobInterview.deleted_at == None).order_by(desc(JobInterview.interview_date))
+        result = await session.exec(statement)
+        return result.all()
+
+    async def get_interviews_by_id(self, job_application_id:str,job_interview_id:str,session:AsyncSession):
+        statement = select(JobInterview).where(JobInterview.job_application_id == job_application_id, JobInterview.id == job_interview_id, JobInterview.deleted_at == None)
+        result = await session.exec(statement)
+        job_interview = result.first()
+        return job_interview if job_interview is not None else None
+    
+    async def create_job_interview(self, interview_data:JobInterviewCreateModel, job_application_id:uuid.UUID, session:AsyncSession):
+        job_interview_dict = interview_data.model_dump()
+        
+        new_data = JobInterview(
+            **job_interview_dict
+        )
+        
+        new_data.job_application_id = job_application_id
+        session.add(new_data)
+        await session.commit()
+        return new_data
+    
+    async def update_job_interview(self, job_application_id:str, job_interview_id:str,update_data:JobInterviewUpdateModel,session: AsyncSession):
+        job_interview_to_update = await self.get_interviews_by_id(job_application_id,job_interview_id,session)
+        if job_interview_to_update is not None:
+            update_data_dict = update_data.model_dump(exclude_unset=True)
+            for key,value in update_data_dict.items():
+                setattr(job_interview_to_update,key,value)
+
+            await session.commit()
+            return job_interview_to_update
+        else:
+            return None
+    
+    async def delete_job_interview(self, job_application_id:str, job_interview_id, session:AsyncSession):
+        job_interview_to_delete = await self.get_interviews_by_id(job_application_id, job_interview_id,session)
+        if job_interview_to_delete is not None:
+            job_interview_to_delete.deleted_at = datetime.now()
+            await session.commit()
+            return job_interview_to_delete
+        else:
+            return None
