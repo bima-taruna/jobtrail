ed0affa2cde6e8e61c3a1f3f62f68394f611d91d
bima-taruna <bimataruna21@gmail.com>
2025-09-10 19:59:18 +0700

    add middleware

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from src.job_application.routes import job_application_router
 from src.auth.routes import auth_router
+from src.interview.routes import job_interview_router
 from contextlib import asynccontextmanager
 from src.db.main import init_db
+from .middleware import register_middleware
 @asynccontextmanager
 async def life_span(app:FastAPI):
     print(f"server is starting ... ")
@@ -10,8 +12,6 @@ async def life_span(app:FastAPI):
     yield
     print(f"server has been stopped ... ")
     
-    
-
 version = "v1"
 
 app = FastAPI(
@@ -19,5 +19,8 @@ app = FastAPI(
     version=version,
 )
 
+register_middleware(app)
+
 app.include_router(job_application_router, prefix=f"/api/{version}/job_applications", tags=["job_applications"])
 app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=["Auth"])
+app.include_router(job_interview_router, prefix=f"/api/{version}", tags=["Job Interviews"])
