1095288019a180fe3ae4be6bb0fb7782559713a2
bima-taruna <bimataruna21@gmail.com>
2025-09-10 19:58:40 +0700

    add index to job application model

--- src/db/models.py
+++ src/db/models.py
@@ -2,6 +2,7 @@ from sqlmodel import SQLModel, Field, Column, Relationship
 import sqlalchemy.dialects.postgresql as pg
 from src.job_application.schemas import Status
 from src.interview.schemas import JobInterviewType
+from sqlalchemy.orm import Mapped
 from datetime import datetime, date
 from typing import Optional, List
 from src.auth.schemas import UserTypes
@@ -9,6 +10,7 @@ from sqlalchemy import Enum as PgEnum
 import uuid
 
 class JobApplication(SQLModel, table=True) :
+    model_config = {"arbitrary_types_allowed": True} # type: ignore[assignment]
     __tablename__ = "job_application" # type: ignore[assignment]
     id: uuid.UUID = Field(
         sa_column=Column(
@@ -18,15 +20,15 @@ class JobApplication(SQLModel, table=True) :
           default=uuid.uuid4  
         )
     )
-    job_title:str
-    company_name:str
+    job_title:str = Field(index=True)
+    company_name:str = Field(index=True)
     location:str
-    application_date:date
-    status : Status
-    user_uid:Optional[uuid.UUID] = Field(default=None, foreign_key="users.id")
+    application_date:date = Field(index=True)
+    status : Status = Field(index=True, default=Status.APPLIED)
+    user_uid:Optional[uuid.UUID] = Field(index=True,default=None, foreign_key="users.id")
     created_at:datetime =  Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
     updated_at:datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
-    deleted_at: Optional[datetime] = None
+    deleted_at: Optional[datetime] = Field(index=True, default=None)
     user: Optional["User"] = Relationship(back_populates="job_applications")
     job_interviews: List["JobInterview"] = Relationship(back_populates="job_application", sa_relationship_kwargs={"lazy":"selectin"})
 
