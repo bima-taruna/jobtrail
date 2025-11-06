122d1c99b432f5f057c6eaddd4ac97a502c99bd4
bima-taruna <bimataruna21@gmail.com>
2025-11-06 21:22:19 +0700

    refact the route to use the dependency

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -1,6 +1,8 @@
-from fastapi import Request, status,Depends
+from fastapi import Request, status, Depends
 from fastapi.security import HTTPBearer
 from fastapi.security.http import HTTPAuthorizationCredentials
+
+from src.interview.service import JobInterviewService
 from .utils import decode_token
 from fastapi.exceptions import HTTPException
 from src.db.redis import token_in_blocklist
@@ -14,99 +16,102 @@ from src.job_timeline.services import JobTimelineService
 
 user_service = UserService()
 
+
 class TokenBearer(HTTPBearer):
-    def __init__(self, auto_error =  True):
+    def __init__(self, auto_error=True):
         super().__init__(auto_error=auto_error)
-        
+
     async def __call__(self, request: Request) -> HTTPAuthorizationCredentials | None:
         creds = await super().__call__(request)
         if creds is None or creds.credentials is None:
             raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="Not authenticated"
+                status_code=status.HTTP_403_FORBIDDEN, detail="Not authenticated"
             )
         token = creds.credentials
         token_data = decode_token(token)
         if not self.token_valid(token):
             raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="Invalid or expired token"
+                status_code=status.HTTP_403_FORBIDDEN, detail="Invalid or expired token"
             )
-        
+
         # if token_data['refresh']:
         #     raise HTTPException(
         #         status_code=status.HTTP_403_FORBIDDEN,
         #         detail="Please provide an access token"
         #     )
-        
-        if await token_in_blocklist(token_data['jti']):
+
+        if await token_in_blocklist(token_data["jti"]):
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail={
-                    "error" :"Token is invalid or has been revoked",
-                    "resolution": "Please get a new token" 
-                }
-            ) 
-        self.verify_token_data(token_data)    
-        
-        return token_data # type: ignore
-    
-    
-    def token_valid(self, token:str)->bool:
+                    "error": "Token is invalid or has been revoked",
+                    "resolution": "Please get a new token",
+                },
+            )
+        self.verify_token_data(token_data)
+
+        return token_data  # type: ignore
+
+    def token_valid(self, token: str) -> bool:
         token_data = decode_token(token)
         if token_data is not None:
             return True
         else:
             return False
-        
+
     def verify_token_data(self, token_data):
-        raise  NotImplementedError('Please override this method in child classes')
-        
+        raise NotImplementedError("Please override this method in child classes")
+
+
 class AccessTokenBearer(TokenBearer):
-    def verify_token_data(self, token_data:dict)->None:
+    def verify_token_data(self, token_data: dict) -> None:
         if token_data.get("refresh", False):
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
-                detail="Please provide an access token"
+                detail="Please provide an access token",
             )
 
 
 class RefreshTokenBearer(TokenBearer):
-    def verify_token_data(self, token_data:dict)->None:
+    def verify_token_data(self, token_data: dict) -> None:
         if not token_data.get("refresh", False):
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
-                detail="Please provide an refresh token"
+                detail="Please provide an refresh token",
             )
 
 
 async def get_current_user(
-    token_detail:dict = Depends(AccessTokenBearer()),
-    session:AsyncSession = Depends(get_session)
-    ):
-    user_email = token_detail['user']['email']
+    token_detail: dict = Depends(AccessTokenBearer()),
+    session: AsyncSession = Depends(get_session),
+):
+    user_email = token_detail["user"]["email"]
     user = await user_service.get_user_by_email(user_email, session)
     return user
 
+
 class RoleChecker:
-    def __init__(self, allowed_roles:List[str]) -> None:
+    def __init__(self, allowed_roles: List[str]) -> None:
         self.allowed_roles = allowed_roles
-    
-    def __call__(self, current_user:User=Depends(get_current_user)) -> Any:
+
+    def __call__(self, current_user: User = Depends(get_current_user)) -> Any:
         if current_user.user_type.value in self.allowed_roles:
             return True
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail="you are not permitted to perform this action"
+            detail="you are not permitted to perform this action",
         )
+
 
 def get_timeline_service() -> JobTimelineService:
     return JobTimelineService()
-        
+
+
 def get_job_service(
-     timeline_service: JobTimelineService = Depends(get_timeline_service)
-    ) -> JobApplicationService:
-    return JobApplicationService(timeline_service = timeline_service)
-        
+    timeline_service: JobTimelineService = Depends(get_timeline_service),
+) -> JobApplicationService:
+    return JobApplicationService(timeline_service=timeline_service)
 
 
+def get_interview_service() -> JobInterviewService:
+    return JobInterviewService()
--- src/interview/routes.py
+++ src/interview/routes.py
@@ -1,61 +1,127 @@
+from typing import Annotated
 from fastapi import APIRouter, status, Depends
 from fastapi.exceptions import HTTPException
 from src.db.main import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
-from src.auth.dependencies import AccessTokenBearer, RoleChecker, get_current_user
+from src.auth.dependencies import (
+    AccessTokenBearer,
+    RoleChecker,
+    get_current_user,
+    get_interview_service,
+)
 from .service import JobInterviewService
 from src.db.models import JobInterview, User
-from .schemas import JobInterviewCreateModel,JobInterviewUpdateModel
+from .schemas import JobInterviewCreateModel, JobInterviewUpdateModel
+import uuid
 
 access_token_bearer = AccessTokenBearer()
 job_interview_router = APIRouter(
-    prefix="/job-applications/{job_application_id}/interviews",
-    tags=["Job Interviews"]
+    prefix="/job-applications/{job_application_id}/interviews", tags=["Job Interviews"]
 )
-job_interview_service = JobInterviewService()
-role_checker_standard = Depends(RoleChecker(['ADMIN', 'USER', 'GUEST']))
-role_checker_admin = Depends(RoleChecker(['ADMIN']))
+JobInterviewServices = Annotated[JobInterviewService, Depends(get_interview_service)]
+SessionDependency = Annotated[AsyncSession, Depends(get_session)]
+UserDependency = Annotated[User, Depends(get_current_user)]
+role_checker_standard = Depends(RoleChecker(["ADMIN", "USER", "GUEST"]))
+role_checker_admin = Depends(RoleChecker(["ADMIN"]))
 
-@job_interview_router.get("/", response_model=list[JobInterview], dependencies=[role_checker_standard])
-async def get_all_job_interviews(job_application_id:str,session:AsyncSession = Depends(get_session), current_user: User = Depends(get_current_user)):
-    job_interviews =  await job_interview_service.get_all_interviews_by_app_id(job_application_id, current_user.id,session)
+
+@job_interview_router.get(
+    "/", response_model=list[JobInterview], dependencies=[role_checker_standard]
+)
+async def get_all_job_interviews(
+    job_application_id: str,
+    job_interview_service: JobInterviewServices,
+    session: SessionDependency,
+    current_user: UserDependency,
+):
+    job_interviews = await job_interview_service.get_all_interviews_by_app_id(
+        job_application_id, current_user.id, session
+    )
     return job_interviews
 
-@job_interview_router.get("/{job_interview_id}", response_model=JobInterview, dependencies=[role_checker_standard])
-async def get_job_interview_by_id(job_application_id:str, job_interview_id:str, session:AsyncSession = Depends(get_session), current_user: User = Depends(get_current_user)):
-    job_interview = job_interview_service.get_interviews_by_id(job_application_id,job_interview_id,current_user.id,session)
-    if job_interview is not None :
+
+@job_interview_router.get(
+    "/{job_interview_id}",
+    response_model=JobInterview,
+    dependencies=[role_checker_standard],
+)
+async def get_job_interview_by_id(
+    job_application_id: str,
+    job_interview_id: str,
+    job_interview_service: JobInterviewServices,
+    session: SessionDependency,
+    current_user: UserDependency,
+):
+    job_interview = job_interview_service.get_interviews_by_id(
+        job_application_id, job_interview_id, current_user.id, session
+    )
+    if job_interview is not None:
         return job_interview
-    else :
+    else:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="interview not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail="interview not found"
         )
 
-@job_interview_router.post("/", status_code=status.HTTP_201_CREATED, response_model=JobInterview, response_model_exclude_none=True, dependencies=[role_checker_standard])
-async def create_job_application(job_application_id,interview_data:JobInterviewCreateModel, session:AsyncSession = Depends(get_session),current_user: User = Depends(get_current_user) ) :
-    job_interview = await job_interview_service.create_job_interview(interview_data,job_application_id, current_user.id, session)
+
+@job_interview_router.post(
+    "/",
+    status_code=status.HTTP_201_CREATED,
+    response_model=JobInterview,
+    response_model_exclude_none=True,
+    dependencies=[role_checker_standard],
+)
+async def create_job_application(
+    job_application_id: uuid.UUID,
+    interview_data: JobInterviewCreateModel,
+    job_interview_service: JobInterviewServices,
+    session: SessionDependency,
+    current_user: UserDependency,
+):
+    job_interview = await job_interview_service.create_job_interview(
+        interview_data, job_application_id, current_user.id, session
+    )
     return job_interview
 
-@job_interview_router.patch("/{interview_id}", response_model_exclude_none=True, dependencies=[role_checker_standard])
-async def update_job_application(job_application_id:str,job_interview_id : str,update_data:JobInterviewUpdateModel,session:AsyncSession = Depends(get_session),current_user: User = Depends(get_current_user)) :
-    update_interview_data = await job_interview_service.update_job_interview(job_application_id,job_interview_id,current_user.id,update_data,session)
-    
+
+@job_interview_router.patch(
+    "/{interview_id}",
+    response_model_exclude_none=True,
+    dependencies=[role_checker_standard],
+)
+async def update_job_application(
+    job_application_id: str,
+    job_interview_id: str,
+    update_data: JobInterviewUpdateModel,
+    job_interview_service: JobInterviewServices,
+    session: SessionDependency,
+    current_user: UserDependency,
+):
+    update_interview_data = await job_interview_service.update_job_interview(
+        job_application_id, job_interview_id, current_user.id, update_data, session
+    )
+
     if update_interview_data:
         return update_data
-    else :
-        raise HTTPException(
-            status_code=404,
-            detail="Interview not found"
-        )
+    else:
+        raise HTTPException(status_code=404, detail="Interview not found")
 
-@job_interview_router.delete("/{interview_id}", status_code=status.HTTP_204_NO_CONTENT, dependencies=[role_checker_standard])
-async def delete_job_application(job_application_id:str,interview_id:str,session:AsyncSession = Depends(get_session),current_user: User = Depends(get_current_user) ) :
-    delete_data = await job_interview_service.delete_job_interview(job_application_id, interview_id, current_user.id, session)
+
+@job_interview_router.delete(
+    "/{interview_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    dependencies=[role_checker_standard],
+)
+async def delete_job_application(
+    job_application_id: str,
+    interview_id: str,
+    job_interview_service: JobInterviewServices,
+    session: SessionDependency,
+    current_user: UserDependency,
+):
+    delete_data = await job_interview_service.delete_job_interview(
+        job_application_id, interview_id, current_user.id, session
+    )
     if delete_data:
         return None
-    else :
-        raise HTTPException(
-            status_code=404,
-            detail="Interview not found"
-        ) 
+    else:
+        raise HTTPException(status_code=404, detail="Interview not found")
